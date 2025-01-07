b9bec8bf13540693f28e19b8204bb686f3b921b2
Sabra Kozlina <sjkozlina@gmail.com>
2025-01-07 14:19:04 -0700

    add, told, untold, delete joke working

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState, useEffect } from "react"
-import { getJokes, addJoke } from "./services/jokeService.js"
+import { getJokes, addJoke, editJoke, deleteJoke } from "./services/jokeService.js"
 import "./App.css"
+import stevePic from "./assets/steve.png"
 
 
 export const App = () => {
   const [allJokes, setAllJokes] = useState([])
   const [newJoke, setNewJoke] = useState("")
+  const [toldJokes, setToldJokes] = useState([])
+  const [untoldJokes, setUntoldJokes] = useState([])
 
-
-  useEffect(() => {
-    getAllJokes()
-  }, [])
-
-  const getAllJokes = () => {
-    getJokes().then(jokesArray => {
-      setAllJokes(jokesArray)
+  const updateDatabase = () => {
+    getJokes().then((jokeArr) => {
+      setAllJokes(jokeArr)
     })
   }
+
+  useEffect(() => {
+    updateDatabase()
+  }, [])
 
   const handleAddJoke = () => {
 
@@ -29,31 +31,86 @@ export const App = () => {
 
       addJoke(jokeToSave).then(() => {
         setNewJoke("")
-        getAllJokes()
+        updateDatabase()
       })
     }
-    }
+  }
+
+  useEffect(() => {
+    const told = allJokes.filter((joke) => joke.told === true)
+    const untold = allJokes.filter((joke) => joke.told === false)
+
+    setToldJokes(told)
+    setUntoldJokes(untold)
+  }, [allJokes])
+
+  const switchToldStatus = (jokeId) => {
+    const updatedJokes = allJokes.map(joke => joke.id === jokeId ? { ...joke, told: !joke.told } : joke)
+    setAllJokes(updatedJokes)
+    const updatedJoke = updatedJokes.find(joke => joke.id === jokeId)
+    editJoke(updatedJoke)
+  }
+
+  const handleDeleteJoke = (jokeId) => {
+    deleteJoke(jokeId).then(() => {
+      setAllJokes(allJokes.filter(joke => joke.id !== jokeId))
+    })
+  }
 
   return (
     <div className="app-container">
       <div className="app-heading">
         <h1 className="app-heading-text">Chuckle Checklist</h1>
+        <div className="app-heading-circle">
+          <img className="app-logo" src={stevePic} alt="Good job Steve" />
+        </div>
       </div>
       <h2>Add Joke</h2>
       <div className="joke-add-form">
-        <input 
-          className="joke-input" 
-          type="text" 
+        <input
+          className="joke-input"
+          type="text"
           value={newJoke}
-          placeholder="New One Liner" 
+          placeholder="New One Liner"
           onChange={(event) => {
             setNewJoke(event.target.value)
-        }} />
+          }} />
 
-        <button 
-          className="joke-input-submit" 
+        <button
+          className="joke-input-submit"
           onClick={handleAddJoke}>Add</button>
+      </div>
 
+      <div className="joke-lists-container">
+        <div className="joke-list-container">
+          <h2>Untold<span className="untold-count">{untoldJokes.length}</span></h2>
+          {untoldJokes.map((joke) => (
+            <div key={joke.id} className="joke-list-item">
+              <p className="joke-list-item-text">{joke.text}</p>
+              <div className="joke-list-action-toggle">
+                <button onClick={() => switchToldStatus(joke.id)}>Told</button>
+              </div>
+              <div className="joke-list-action-delete">
+                <button onClick={() => handleDeleteJoke(joke.id)}>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="joke-list-container">
+          <h2>Told<span className="told-count">{toldJokes.length}</span></h2>
+          {toldJokes.map((joke) => (
+            <div key={joke.id} className="joke-list-item">
+              <p className="joke-list-item-text">{joke.text}</p>
+              <div className="joke-list-action-toggle">
+                <button onClick={() => switchToldStatus(joke.id)}>Untold</button>
+              </div>
+              <div className="joke-list-action-delete">
+                <button onClick={() => handleDeleteJoke(joke.id)}>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -12,3 +12,24 @@ export const addJoke = (joke) => {
     }
     return fetch("http://localhost:8088/jokes", postOptions)
 }
+
+export const editJoke = (joke) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(joke)
+    }
+    return fetch(`http://localhost:8088/jokes/${joke.id}`, putOptions)
+}
+
+export const deleteJoke = (joke) => {
+    const deleteOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    return fetch(`http://localhost:8088/jokes/${joke}`, deleteOptions)
+}
